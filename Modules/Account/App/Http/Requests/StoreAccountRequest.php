e9b88554aba4a098e85c517124ce26724e964048
venilson2 <venilson2009@hotmail.com>
2024-10-22 22:33:34 -0300

    feat(account): StoreAccountRequest for account validation

---
+++ Modules/Account/App/Http/Requests/StoreAccountRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Account\App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAccountRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        return [
+            'title'         => 'required|string|max:255',
+            'description'   => 'nullable|string',
+            'value'         => 'required|numeric',
+            'due_date'      => 'required|date',
+            'status'        => 'required|in:paid,pending',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
